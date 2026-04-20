cbcf5e3b7332b32e24ed6aaaacdf345815e313e0
Pierre-Yves PARANTHOEN <nuxsfm@gmail.com>
2026-04-20 19:32:08 +0200

    add author header to all scripts

--- dd_wipe.py
+++ dd_wipe.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# Author: Pierre-Yves PARANTHOEN
 
 import os
 import sys
--- run_wipe.sh
+++ run_wipe.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Author: Pierre-Yves PARANTHOEN
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
